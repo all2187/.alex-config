89a930760e05006a3c9ee1d382e8196b6d4b27a8
Alexandre Lamy <all2187@columbia.edu>
2016-12-27 18:50:56 -0500

    Modified setup.sh to create .backup dir

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cd ~ && rm -f .vimrc && rm -f .bashrc && ln -s .alex-config/.vimrc .vimrc && ln -s .alex-config/.bashrc .bashrc 
+cd ~ && rm -f .vimrc && rm -f .bashrc && ln -s .alex-config/.vimrc .vimrc && ln -s .alex-config/.bashrc .bashrc && mkdir -p .backup 
 
 
